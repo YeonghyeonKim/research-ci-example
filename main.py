43fe58cd32b58ef70e32bae943a02288cde00041
kimyh <kimyh@fasol.co.kr>
2021-09-03 10:21:55 +0900

    dup

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ def helloworld(a):
     print(msg)
     return msg
 
+def helloworld(a):
+    msg = "hello world!" + a
+    print(msg)
+    return msg
+
 
 if __name__ == "__main__": # pragma: no cover
     helloworld("alalal")
